3734202f81e658d5f02bd5875126c13de0b94eab
Avielle Lewis <aviselanj@gmail.com>
2024-06-15 23:48:12 -0500

    initial setup connecting to backend repo

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// @ts-ignore
+ function aboutPage(props) {
+    return (
+        <>
+            <h1>
+                Welcome to Big Easy Events
+            </h1>
+        </>
+    );
+}
+export default aboutPage
